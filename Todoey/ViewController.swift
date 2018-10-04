a72bcde003608a41ae4dbb91a9ba47d41aa7864f
Bekzhan <bekzhan.kaspakov@nu.edu.kz>
2018-10-04 21:25:20 +0600

    first commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Bekzhan Kaspakov on 10/4/18.
+//  Copyright © 2018 Bekzhan Kaspakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
